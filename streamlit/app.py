8791156841a86643b0997da17f6c041a8aed4d29
SitwalaM <47774622+SitwalaM@users.noreply.github.com>
2022-05-03 10:18:18 +0200

    updated the app

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,4 +1,18 @@
 import streamlit as st
+import s3fs
+import os
+import pandas as pd
 
 
-st.title("Hello World")
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+
+
+fs = s3fs.S3FileSystem(anon=False)
+bytes_to_write = df.to_csv(None).encode()
+
+with fs.open('s3://salonanalytics/myfile.csv', 'wb') as f:
+    f.write(bytes_to_write)
+
