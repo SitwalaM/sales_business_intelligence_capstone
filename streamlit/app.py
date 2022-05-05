ead8898c8937669091ca44ae4b8bbf6635a9c0fb
SitwalaM <47774622+SitwalaM@users.noreply.github.com>
2022-05-05 07:39:56 +0200

    updated streamlit app

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,17 +2,15 @@ import streamlit as st
 import s3fs
 import os
 import pandas as pd
+from datetime import date
+ 
 
-
-
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Choose a file", type = ['csv']) 
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
 
 
-fs = s3fs.S3FileSystem(anon=False)
-bytes_to_write = df.to_csv(None).encode()
-
-with fs.open('s3://salonanalytics/myfile.csv', 'wb') as f:
-    f.write(bytes_to_write)
+  fs = s3fs.S3FileSystem(anon=False)
+  filename = str(date.today()) + ".csv"
+  df.to_csv('s3://salonanalytics/'+filename)
 
