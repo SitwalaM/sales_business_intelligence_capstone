4b690ba016aab6a510a727a4ed93d3dc9a52d286
SitwalaM <47774622+SitwalaM@users.noreply.github.com>
2022-05-06 18:52:58 +0200

    added KPI logic

--- streamlit/app.py
+++ streamlit/app.py
@@ -5,27 +5,64 @@ import pandas as pd
 from datetime import date
 import mysql.connector
 from sqlalchemy import create_engine
+from app_utilities import *
  
 
-uploaded_file = st.file_uploader("Upload Latest Sales Data", type = ['csv']) 
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
+st.title("Sales Dashboard")
 
-
-  fs = s3fs.S3FileSystem(anon=False)
-  filename = str(date.today()) + ".csv"
-  df.to_csv('s3://salonanalytics/'+filename)
-
-
+# Database connection
 @st.experimental_singleton
 def init_connection():
+
     return create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
                                                format(st.secrets["mysql"]["user"], st.secrets["mysql"]["password"], 
                                                       st.secrets["mysql"]["host"], st.secrets["mysql"]["database"]), pool_recycle=1, pool_timeout=57600).connect()
-
 conn = init_connection()
 
-query = "SELECT * from forecasts;"
+#@st.experimental_memo(ttl=600)
+def run_query(query, conn):
+  # runs query and returns dataframe
 
-forecasts_df = pd.read_sql(query, conn)
-st.dataframe(result_dataFrame)
+  df = pd.read_sql(query,conn)
+
+  return df
+
+
+query1 = "SELECT * from forecasts;"
+query2 = "SELECT * from transactions;"
+
+#------------ Read in main dataframes and transformations---------------------------
+forecasts_df = run_query(query1,conn)
+transactions_df = run_query(query2,conn)
+monthly_grouped_sales = group_by_month(transactions_df)
+customer_count_per_month = get_customers_per_month(transactions_df)
+
+
+#------------------------ Sales Metrics-----------------------------------------------
+dates = get_date_references(transactions_df)
+sales_today = get_total_day(transactions_df ,dates["today"])
+sales_yesterday = get_total_day(transactions_df ,dates["yesterday"])
+day_delta = sales_today - sales_yesterday
+month_sales_now = monthly_grouped_sales.iloc[-1,0]
+month_sales_last_month = monthly_grouped_sales.iloc[-2,0]
+unique_customers_now = customer_count_per_month.iloc[-1,0]
+unique_customers_last_month = customer_count_per_month.iloc[-2,0]
+delta_customer_count =  unique_customers_now - unique_customers_last_month 
+
+#-------------------------- Titel and Metrics------------------------------------
+st.markdown('### KPIs')
+col1, col2, col3 = st.columns(3)
+col1.metric(label = "Sales Today", value = "K "+ str(sales_today) , delta = day_delta)
+col2.metric("Sales this Month", value = month_sales_now, delta = month_sales_now - month_sales_last_month )
+col3.metric("Customer Count", value = unique_customers_now , delta = delta_customer_count.item())
+
+
+#-------------------------Upload latest data --------------------------------------
+with st.sidebar:
+  uploaded_file = st.file_uploader("Upload Latest Sales Data", type = ['csv']) 
+  if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    fs = s3fs.S3FileSystem(anon=False)
+    filename = str(date.today()) + ".csv"
+    df.to_csv('s3://salonanalytics/'+filename)
+
---
+++ streamlit/app_utilities.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+
+def group_by_month(df):
+
+    df = df.sort_values("Date", ascending = True)
+    df["month-year"] =  df["Date"].apply(lambda x: x.strftime('%Y-%m')) 
+    #df = df.groupby("month-year", as_index = False).sum()
+    df = pd.pivot_table(df, index = 'month-year', values = 'Total', aggfunc='sum')
+
+    return df
+
+    
+def get_date_references(df):
+
+    today =df.Date.max() #datetime.strptime(df.Date.max(), '%Y-%m-%d %H:%M:%S')
+    yesterday = today  - timedelta(days=1)
+
+    return {"today": today.strftime('%Y-%m-%d'), "yesterday": yesterday.strftime('%Y-%m-%d')}
+
+
+def get_total_day(df,day):
+
+    df = df.copy()
+    df["Date"] =  df["Date"].apply(lambda x: x.strftime('%Y-%m-%d')) 
+    df = df[df["Date"]==day]
+    sales = df.Total.sum()
+
+    return sales
+
+
+def get_customers_per_month(df):
+
+    df = df.copy()
+    df["Date"] =  df["Date"].apply(lambda x: x.strftime('%Y-%m')) 
+    df = pd.pivot_table(df, index = 'Date', values = 'Customer', aggfunc='nunique')
+
+    return df
+
+
+
