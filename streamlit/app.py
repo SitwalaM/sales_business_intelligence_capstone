318406fbbab79c4bc665825a4272dc7f73dc0ed0
SitwalaM <47774622+SitwalaM@users.noreply.github.com>
2022-05-09 06:06:09 +0200

    added forecast dataframe

--- streamlit/app.py
+++ streamlit/app.py
@@ -37,6 +37,7 @@ forecasts_df = run_query(query1,conn)
 transactions_df = run_query(query2,conn)
 monthly_grouped_sales = group_by_month(transactions_df)
 customer_count_per_month = get_customers_per_month(transactions_df)
+filtered_forecast = get_forecast_month(forecasts_df,transactions_df)
 
 
 #------------------------ Sales Metrics-----------------------------------------------
@@ -49,14 +50,17 @@ month_sales_last_month = monthly_grouped_sales.iloc[-2,0]
 unique_customers_now = customer_count_per_month.iloc[-1,0]
 unique_customers_last_month = customer_count_per_month.iloc[-2,0]
 delta_customer_count =  unique_customers_now - unique_customers_last_month 
+total_forecast = filtered_forecast.yhat.sum()
+forecast_month = filtered_forecast.ds.max().strftime("%b")
 
-#-------------------------- Titel and Metrics------------------------------------
+#-------------------------- Title and Metrics------------------------------------
 st.markdown('### KPIs')
 col1, col2, col3, col4 = st.columns(4)
-col1.metric(label = "Sales Today", value = "K "+ str(sales_today) , delta = day_delta)
-col2.metric("Sales this Month", value = month_sales_now, delta = month_sales_now - month_sales_last_month )
-col3.metric("Customer Count", value = unique_customers_now , delta = delta_customer_count.item())
-col4.metric("Sales Forecast", value = unique_customers_now , delta = delta_customer_count.item())
+col1.metric(label = "Sales Today", value = "K "+ f"{sales_today:,}", delta = day_delta)
+col2.metric("Sales this Month", value = f"{month_sales_now:,}", delta = month_sales_now - month_sales_last_month )
+col3.metric("Sales Forecast:  " +  f"{forecast_month:,}", value = total_forecast )
+col4.metric("Customer Count", value = f"{unique_customers_now:,}", delta = delta_customer_count.item())
+
 
 
 #-------------------------Upload latest data --------------------------------------
@@ -74,3 +78,4 @@ st.plotly_chart(monthly_sales)
 
 customer_count = px.line(customer_count_per_month, x = monthly_grouped_sales.index , y = "Customer")
 st.plotly_chart(customer_count)
+#st.write(t)
--- streamlit/app_utilities.py
+++ streamlit/app_utilities.py
@@ -1,19 +1,43 @@
 import pandas as pd
 from datetime import datetime, timedelta
+import calendar
+from dateutil.relativedelta import *
 
 
 
 def group_by_month(df):
+    '''
+    takes transaction dataframe and groups by month
+
+    Parameters
+    ----------
+    df: pandas dataframe of transactions
+
+    Returns
+    ---------
+    df: pandas dataframe pivoted by the month/year
+    '''
 
     df = df.sort_values("Date", ascending = True)
     df["month-year"] =  df["Date"].apply(lambda x: x.strftime('%Y-%m')) 
-    #df = df.groupby("month-year", as_index = False).sum()
     df = pd.pivot_table(df, index = 'month-year', values = 'Total', aggfunc='sum')
 
     return df
 
     
 def get_date_references(df):
+    '''
+    Returns the current date and date yesterday based on latest queried data
+
+    Parameters
+    ----------
+    df: pandas dataframe of transactions
+
+    Returns
+    --------
+    dictionary >> {"today": date, "yesterday": date}
+
+    '''
 
     today =df.Date.max() #datetime.strptime(df.Date.max(), '%Y-%m-%d %H:%M:%S')
     yesterday = today  - timedelta(days=1)
@@ -22,6 +46,19 @@ def get_date_references(df):
 
 
 def get_total_day(df,day):
+    '''
+    gets the total sales for a particular dat
+
+    Parameters
+    ----------
+    df: pandas dataframe of transactions
+
+    day: string format date
+
+    Returns
+    --------
+    sales: float - total sales for day
+    '''
 
     df = df.copy()
     df["Date"] =  df["Date"].apply(lambda x: x.strftime('%Y-%m-%d')) 
@@ -32,6 +69,17 @@ def get_total_day(df,day):
 
 
 def get_customers_per_month(df):
+    '''
+    counts number of customers per month
+
+    Parameters
+    ----------
+    df: pandas dataframe of transactions
+
+    Returns
+    -------
+    df: pandas dataframe pivoted by month/year and number of unique customers
+    '''
 
     df = df.copy()
     df["Date"] =  df["Date"].apply(lambda x: x.strftime('%Y-%m')) 
@@ -40,11 +88,34 @@ def get_customers_per_month(df):
     return df
 
 
-def get_forecast_total(df, last_day):
+def get_forecast_month(df_forecast,df_transactions):
+    '''
+    filters predictions dataframe to only show pending days in the month or
+    if it's the end of the month, show predictions for next month
 
-    df = df.copy()
-    
+    Parameters
+    ----------
+    df_forecast: pandas dataframe output from Prophet model
 
+    df_transactions: transactions dataframe
 
-    return forecast_total
+    Returns
+    ---------
+    df_future: pandas dataframe of filtered Prophet model
+    '''
+
+    df_future = df_forecast.copy()
+    df_future["ds"] = df_future["ds"].apply(lambda x:  datetime.strptime(str(x), '%Y-%m-%d %H:%M:%S'))
+    max_date = df_transactions.loc[df_transactions.index[-1],"Date"]
+
+    if max_date.day ==  calendar.monthrange(max_date.year, max_date.month)[1]:
+        max_filter =  max_date + relativedelta(months=+1)
+    else:
+        days_delta = calendar.monthrange(max_date.year, max_date.month)[1] -  max_date.day
+        max_filter = max_date + relativedelta(days=days_delta)
+
+    df_future = df_future[(df_future["ds"] <= max_filter) & (df_future["ds"] > max_date)]
+        
+
+    return df_future
 
